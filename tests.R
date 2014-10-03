da9c0d3310c6f2791edc66ab87e72cb0dd1050a8
Sebastian Popa <spopa74@gmail.com>
2014-10-03 14:36:49 -0400

    Some small modifs

--- tests.R
+++ tests.R
@@ -1,15 +1,20 @@
 start <- proc.time()
 
-tokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 5))
+## Java stuff
+Sys.setenv(JAVA_HOME="")
+options(java.parameters="-Xmx14g")
 
-en_texts <- VCorpus(DirSource(directory="data/en_US/small", encoding="UTF-8"), 
+## load RWeka, tm libs
+library(RWeka)
+library(tm)
+
+## create a tokenizer for up to 5-Grams
+tokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 5, max = 5))
+en_texts <- VCorpus(DirSource(directory="2_train - Copy/", encoding="UTF-8"), 
                     readerControl=list(language="en"))
-
 en_texts <- tm_map(x=en_texts, FUN=removeNumbers)
-en_texts <- tm_map(x=en_texts, FUN=removePunctuation)
-en_texts <- tm_map(x=en_texts, FUN=tolower)
 
-## options(mc.cores=1)
+## create the TermDocumentMatrix
 tdm <- TermDocumentMatrix(en_texts, control=list(tokenizer=tokenizer))
 
 ## show the running time
