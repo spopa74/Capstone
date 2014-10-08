90ea980b26f709262777ea166ca48a1621eb8631
Sebastian Popa <spopa74@gmail.com>
2014-10-08 01:01:39 -0400

    created files for the N-gram models

--- processTDMs.R
+++ processTDMs.R
@@ -10,12 +10,144 @@ library(RWeka)
 library(tm)
 
 ## load a previously saved TermDocumentMatrix
-load("blog.tdm")
+load("5.tdm")
 tdmMatrix <- as.matrix(tdm)
 
 ## get the sums over rows
-aggblogs <- rowSums(tdmMatrix)
+tf1grams <- rowSums(tdmMatrix)
+tf1sorted <- sort(tf1grams, decreasing = TRUE)
 
 ## save the vector, has as element names the N-grams (.tf stands for term frequencies)
-save(aggblogs, file="aggblogs.tf")
+save(tf1grams, file="5Grams.tf")
+save(tf1sorted, file="5Sorted.tf")
 
+
+
+
+## break the sorted vector into 4 different vectors, save all of them
+## vector 1, full N-gram (NwordIndex.tf files, e.g. 1wordIndex.tf)
+## vector 2, "prior" = N-1-grams (indexes in the N-1 vectors) (NpriorIx.tf files)
+## vector 3, "posterior" = 1-gram (NposteriorIx.tf files)
+## vector 4, frequency of the N-gram (Nfreq.tf files)
+
+## vectors for 1-Grams (no prior, no posterior)
+wordIndex1Gram <- 1:length(tf1sorted)
+names(wordIndex1Gram) <- names(tf1sorted)
+save(wordIndex1Gram, file="4_models/1wordIndex.tf")
+freq1Gram <- unname(tf1sorted)
+save(freq1Gram, file="4_models/1freq.tf")
+
+## ------------------------ similar for 2-Grams
+len2 <-length(tf2sorted)
+wordIndex2Gram <- 1:len2
+names(wordIndex2Gram) <- names(tf2sorted)
+save(wordIndex2Gram, file="4_models/2wordIndex.tf")
+## find prior, posterior
+allNames2 <- names(tf2sorted)
+allNamesCollapsed2 <- paste(allNames2, collapse = " ")
+allNamesVector2 <- unlist(strsplit(allNamesCollapsed2, split = " "))
+
+prior2 <- allNamesVector2[seq.int(1, len2 * 2, 2)]
+post2 <- allNamesVector2[seq.int(2, len2 * 2, 2)]
+prior2Ix <- wordIndex1Gram[prior2]
+post2Ix <- wordIndex1Gram[post2]
+save(prior2Ix, file="4_models/2priorIx.tf")
+save(post2Ix, file="4_models/2posteriorIx.tf")
+
+tf2sorted <- unname(tf2sorted)
+save(tf2sorted, file="4_models/2freq.tf")
+
+
+## ----------------- similar for 3-Grams
+load("3_grams//3sorted.tf")
+len3 <-length(tf3sorted)
+wordIndex3Gram <- 1:len3
+names(wordIndex3Gram) <- names(tf3sorted)
+save(wordIndex3Gram, file="4_models/3wordIndex.tf")
+## find prior, posterior
+allNames3 <- names(tf3sorted)
+allNamesCollapsed3 <- paste(allNames3, collapse = " ")
+remove(allNames3)
+allNamesVector3 <- unlist(strsplit(allNamesCollapsed3, split = " "))
+remove(allNamesCollapsed3)
+
+## calculate prior, post
+prior3_1 <- allNamesVector3[seq.int(1, len3 * 3, 3)]
+prior3_2 <- allNamesVector3[seq.int(2, len3 * 3, 3)]
+post3 <- allNamesVector3[seq.int(3, len3 * 3, 3)]
+remove(allNamesVector3)
+
+prior3Ix <- wordIndex2Gram[paste(prior3_1, prior3_2, sep = " ")]
+post3Ix <- wordIndex1Gram[post3]
+prior3Ix <- unname(prior3Ix)
+post3Ix <-unname(post3Ix)
+save(prior3Ix, file="4_models/3priorIx.tf")
+save(post3Ix, file="4_models/3posteriorIx.tf")
+
+tf3sorted <- unname(tf3sorted)
+save(tf3sorted, file="4_models/3freq.tf")
+
+
+## ----------------- similar for 4-Grams
+load("3_grams//4sorted.tf")
+len4 <-length(tf4sorted)
+wordIndex4Gram <- 1:len4
+names(wordIndex4Gram) <- names(tf4sorted)
+save(wordIndex4Gram, file="4_models/4wordIndex.tf")
+## find prior, posterior
+allNames4 <- names(tf4sorted)
+allNamesCollapsed4 <- paste(allNames4, collapse = " ")
+remove(allNames4)
+allNamesVector4 <- unlist(strsplit(allNamesCollapsed4, split = " "))
+remove(allNamesCollapsed4)
+
+## calculate prior, post
+prior4_1 <- allNamesVector4[seq.int(1, len4 * 4, 4)]
+prior4_2 <- allNamesVector4[seq.int(2, len4 * 4, 4)]
+prior4_3 <- allNamesVector4[seq.int(3, len4 * 4, 4)]
+post4 <- allNamesVector4[seq.int(4, len4 * 4, 4)]
+remove(allNamesVector4)
+
+prior4Ix <- wordIndex3Gram[paste(prior4_1, prior4_2, prior4_3, sep = " ")]
+post4Ix <- wordIndex1Gram[post4]
+prior4Ix <- unname(prior4Ix)
+post4Ix <-unname(post4Ix)
+save(prior4Ix, file="4_models/4priorIx.tf")
+save(post4Ix, file="4_models/4posteriorIx.tf")
+
+tf4sorted <- unname(tf4sorted)
+save(tf4sorted, file="4_models/4freq.tf")
+
+
+
+## ----------------- similar for 5-Grams
+load("3_grams//5sorted.tf")
+len5 <-length(tf5sorted)
+wordIndex5Gram <- 1:len5
+names(wordIndex5Gram) <- names(tf5sorted)
+save(wordIndex5Gram, file="4_models/5wordIndex.tf")
+tf5sorted <- unname(tf5sorted)
+save(tf5sorted, file="4_models/5freq.tf")
+
+## find prior, posterior
+allNames5 <- names(wordIndex5Gram)
+
+allNamesCollapsed5 <- paste(allNames5, collapse = " ")
+remove(allNames5)
+allNamesVector5 <- unlist(strsplit(allNamesCollapsed5, split = " "))
+remove(allNamesCollapsed5)
+
+## calculate prior, post
+prior5_1 <- allNamesVector5[seq.int(1, len5 * 5, 5)]
+prior5_2 <- allNamesVector5[seq.int(2, len5 * 5, 5)]
+prior5_3 <- allNamesVector5[seq.int(3, len5 * 5, 5)]
+prior5_4 <- allNamesVector5[seq.int(4, len5 * 5, 5)]
+post5 <- allNamesVector5[seq.int(5, len5 * 5, 5)]
+remove(allNamesVector5)
+
+prior5Ix <- wordIndex4Gram[paste(prior5_1, prior5_2, prior5_3, prior5_4, sep = " ")]
+post5Ix <- wordIndex1Gram[post5]
+prior5Ix <- unname(prior5Ix)
+post5Ix <-unname(post5Ix)
+save(prior5Ix, file="4_models/5priorIx.tf")
+save(post5Ix, file="4_models/5posteriorIx.tf")
--- runModels.R
+++ runModels.R
@@ -1,5 +1,7 @@
 ## various functions, run a model over the test data. Measures accuracy (hits/misses)...
 
+library(Matrix)
+
 
 ## ----------------- Load the test file ---------------------------
 ## The test file is big enough, don't want to re-load it every time. 
@@ -63,6 +65,46 @@ run1GramModel <- function(data, n, tf1sorted) {
 
 
 ## -------------- CREATE 2-GRAM model ---------------------
+
+## INPUT: 
+## tf1Sorted - term frequencies for 1-Grams, sorted decreasingly by frequency
+## tf2Sorted - same, for 2-Grams
+## OUTPUT: 
+## 
+create2GramModel <- function(tf1Sorted, tf2Sorted, n) {
+  start <- proc.time()
+  
+  len1 <- length(tf1Sorted)
+  len2 <- length(tf2Sorted)
+  
+  ## create 2 vectors for tf1 (term freq 1-grams)
+  ## indexes (1-gram to index) and frequencies (index to frequency)
+  print("break 1-grams in indexes and frequencies")
+  indexes1Gram <- seq.int(1, length(tf1Sorted))
+  names(indexes1Gram) <- names(tf1Sorted)
+  tf1Sorted <- unname(tf1Sorted)
+  
+  ## break the 2-grams vector into 2 vectors, prior and posterior
+  print("break 2-grams names in 2 vectors, prior and posterior")
+  allWords2Grams <- unlist(strsplit(names(tf2Sorted), split=" "))
+  ## now we have a concatenated list of priors and posteriors
+  priorWords <- allWords2Grams[seq(1, 2 * len2, 2)]
+  posteriorWords <- allWords2Grams[seq(2, 2* len2, 2)]
+  remove(allWords2Grams)
+  ## get the indexes of above words, from tf1sorted
+  priorIx <- indexes1Gram[priorWords]
+  posteriorIx <- indexes1Gram[posteriorWords]
+  
+  ## we have the indexes, frequencies, figure out the best possibilities
+  print("create the sparse matrix with the model")
+  bigramModel <- Matrix(0, nrow=len1, ncol=len1, sparse = TRUE)  
+  bigramModel[priorIx, posteriorIx] <- tf2Sorted / tf1Sorted[priorIx]
+  
+  print(proc.time() - start)
+  return(bigramModel)
+  
+}
+
 ## Based on the sorted counts of 1- and 2-Grams, create the 
 ## structure for holding the best N possibilities following
 ## a certain word. Since I'm using it with 1 or 3, the structure
@@ -71,45 +113,50 @@ run1GramModel <- function(data, n, tf1sorted) {
 ## followups, ordered decreasingly by probability. Quick note: 
 ## since the probability is given by the MLE, then I'll take 
 ## into consideration only the counts. 
-## Structure returned is a vector of:
-## name = index in the 1-gram
-## vector = vector of 3 possible next words (ordered by probability)
-## Parameters:
+## 
+## RETURNS: 
+## Sparse matrix of:
+## row - index in the 1-gram
+## column - 3 possible next words (as indexes in 1-gram)
+## value at row + column - probability
+## 
+## PARAMS:
 ## counts2Gram - a term frequency model of the 2-Grams, sorted by frequency
 ## indexes1Gram - a vector with 1-Grams and indexes
 ## freq1Gram - a vector with indexes and frequencies for 1-Grams
 ## n - how many possibilities for the "next word" (1 or 3 in this case)
-create2GramModel <- function(counts2Gram, indexes1Gram, freq1Gram, n) {
+## NOTE!!!!!!!
+## DO NOT USE, WAY TOO SLOW FOR A REASONABLE SIZE VECTOR (10 million 2-Grams)
+create2GramModelSLOW <- function(counts2Gram, indexes1Gram, freq1Gram, n) {
   start <- proc.time()
-  model <- vector()
-  
   ## loop through the 2-grams
   len2g <- length(counts2Gram)
-  for (i in 1:len2g){
-    nameGram <- names(counts2Gram[i])
-    ## this is a 2 gram, so there is a space
-    words <- unlist(strsplit(nameGram, split = " "))
-    word1 <- words[1]
-    word2 <- words[2]
-    
-    ## check if the structure has a vector associated with word1 
-    vecWord1 <- model[word1]
-    if (is.na(vecWord1)){
-      vecWord1 <- vector()
-      model[word1] <- vecWord1
+  len1g <- length(indexes1Gram)
+  
+  model <- Matrix(0, nrow = len1g, ncol = len1g, sparse = TRUE)
+  
+  for (i in 1:len2g) {
+    ## just a print to see that the code is running
+    if(i %% 10000 == 0) {
+      print(paste("2-Gram: ", counts2Gram[i], ", index: ", i))
     }
     
-    ## check if the respective word doesn't have already the slots taken. 
-    ## since the model is created based on a decreasingly sorted initial
-    ## vectors, if the slots are taken then they are the ones with the
-    ## biggest probabilities, so ignore the rest. 
-    if(length(vecWord1) == n)
-      continue
+    ## get the current 2-gram
+    nameGram <- names(counts2Gram[i])
+    ## this is a 2-gram, so there is a space
+    words <- unlist(strsplit(nameGram, split = " "))
+    indexWord1 <- indexes1Gram[words[1]]
+    indexWord2 <- indexes1Gram[words[2]]
+    
+    ## check if we have already 3 values in the respective line
+    filled <- slotsFilled[indexWord1, 1]
+    if (filled == n)
+      next
     
     ## if we're here, slot not full yet
-    indexWord1 <- tf1sortedIndexes[word1]
-    model[word1] <- c(vecWord1, c(indexes1Gram[word2], counts2Gram[i] / tf1freq[indexWord1])
-    
+    probability <- counts2Gram[i] / tf1freq[indexWord1]
+    model[indexWord1, indexWord2] <- probability
+    slotsFilled[indexWord1, 1] <- filled + 1
   }
   
   print(proc.time() - start)
@@ -133,8 +180,7 @@ run2GramModel <- function(data, n, model, tf2sorted) {
   misses <- 0
   notin <- 0
   
-  for(i in 1:len){
-    
+  for(i in 1:len) {
     ## get the current and next words
     currentWord <- data[i]
     if (i<len) 
